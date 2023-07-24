96958638e46bfd3eeb596ed9a0b191ec1b9597b9
Helder Medeiros <helderberserk@gmail.com>
2023-07-24 20:03:53 -0300

    JSON - Conceito e Função Marshal

--- 01. Fundamentos da Linguagem/20. JSON/main.go
+++ 01. Fundamentos da Linguagem/20. JSON/main.go
@@ -9,8 +9,8 @@ import (
 
 type cachorro struct {
 	Nome  string `json:"nome"`
-	Raca  string `json: "raca"`
-	Idade uint   `json: "idade"`
+	Raca  string `json:"raca"`
+	Idade uint   `json:"idade"`
 }
 
 func main() {
