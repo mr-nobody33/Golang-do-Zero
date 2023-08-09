7ffd0dcf0372565e11f02980e37112a5ece0ac14
Helder Medeiros <helderberserk@gmail.com>
2023-08-09 20:13:52 -0300

    Pausa nos Estudos até instalação do Linux

---
+++ 01. Fundamentos da Linguagem/23. Banco de Dados/banco-de-dados.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql" // Importar de maneira implícia com o anderline na frente
+)
+
+// Pausa nos Estudos até instalação do Linux. AULA 3. CONECTANDO COM O BANCO DE DADOS
+
+func main() {
+	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão está aberta!")
+}
