46cba238f0ae41e5bd9f7b23d3031f3b29a11eed
Mr. Nobody <helderberserk@gmail.com>
2023-08-18 20:01:49 -0300

    Erro banco de dados. Pausa nos estudos.

--- 01. Fundamentos da Linguagem/24. CRUD Básico/main.go
+++ 01. Fundamentos da Linguagem/24. CRUD Básico/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/servidor"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,15 @@ import (
 )
 
 func main() {
+	// CRUD = Create, Read, Update, Delete
+
+	// CREATE = POST
+	// READ = GET
+	// UPDATE = PUT
+	// DELETE = DELETE
+
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
 
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
