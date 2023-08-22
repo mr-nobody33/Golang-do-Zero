1dabddbbb4ea3984ea68b2f0b240e44f199c3a1e
Mr. Nobody <helderberserk@gmail.com>
2023-08-22 15:25:28 -0300

    62. Buscando Dados

--- 01. Fundamentos da Linguagem/24. CRUD Básico/servidor/servidor.go
+++ 01. Fundamentos da Linguagem/24. CRUD Básico/servidor/servidor.go
@@ -61,3 +61,41 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! Id: %d", idInserido)))
 }
+
+// BuscarUsuarios traz todos os usuários salvos no banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar com o banco de dados!"))
+	}
+	defer db.Close()
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		w.Write([]byte("Erro ao buscar usuários!"))
+	}
+	defer linhas.Close()
+
+	var usuarios []usuario
+	for linhas.Next() {
+		var usuario usuario
+
+		if erro := linhas.Scan(&usuario.ID, &usuario.Nome, &usuario.Email); erro != nil {
+			w.Write([]byte("Erro ao escanear o usuário!"))
+			return
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if erro := json.NewEncoder(w).Encode(usuarios); erro != nil {
+		w.Write([]byte("Erro ao converter os usuários para JSON!"))
+		return
+	}
+}
+
+// BuscarUsuario traz um usuário específico salvo no banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+
+}
